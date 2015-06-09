341d63bc9cd0ca46dbd364981ceabe7a86dd7d4d
bowang <orchidwangbo@gmail.com>
2015-06-08 22:28:46 -0400

    rm everything and change to another textbook. The previous one is too easy．

--- GameEntry.cpp
+++
@@ -1,5 +0,0 @@
-GameEntry::GameEntry(const string& n, int s)
-    : name(n), score(s) {}
-
-string GameEntry::getName() const {return name};
-int GameEntry::getScore() const {return score;}
--- twoDimensionalVector.cpp
+++
@@ -1,2 +0,0 @@
-vector< vector<int> > M(n,vector<int>(m));
-cout << M[i][j] << endl;
